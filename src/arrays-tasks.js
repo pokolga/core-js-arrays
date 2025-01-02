8f6bd26204137aa61c1ef8d0f4dc044b1c3b73e3
pokolga <pokolga1801@gmail.com>
2025-01-02 15:25:23 +0300

    feat: 15 passing 34 pending

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -126,8 +126,9 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  return +(arr.reduce((acc, value) => acc + value, 0) / arr.length).toFixed(2);
 }
 
 /**
@@ -140,8 +141,8 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((elem) => elem.length === arr[0].length);
 }
 
 /**
