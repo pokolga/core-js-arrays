d3fddc3ca94cd8163c94acb1e5eda26cd701a7e2
pokolga <pokolga1801@gmail.com>
2025-01-03 13:33:25 +0300

    feat: I did everything!

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -266,8 +266,12 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n === 1) return Array.from({ length: size }).fill(0);
+  const result = Array.from({ length: size }).fill(
+    createNDimensionalArray(n - 1, size)
+  );
+  return result;
 }
 
 /**
